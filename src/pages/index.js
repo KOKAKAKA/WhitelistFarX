318ad708d71af2372462195cab413ff3f06a84fb
Yumi <90940961+KOKAKAKA@users.noreply.github.com>
2024-07-20 15:49:14 +0700

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import KeyInput from '../components/KeyInput';
 import KeyGenerator from '../components/KeyGenerator';
 
 const IndexPage = () => (
   <div>
     <h1>Whitelist System</h1>
-    <KeyInput />
     <KeyGenerator />
   </div>
 );
