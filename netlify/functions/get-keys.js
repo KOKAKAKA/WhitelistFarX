2eb1bb8bc9b115efd1e92737d81267bd3259075c
Yumi <90940961+KOKAKAKA@users.noreply.github.com>
2024-07-21 05:58:41 +0700

    Update get-keys.js

--- netlify/functions/get-keys.js
+++ netlify/functions/get-keys.js
@@ -15,6 +15,20 @@ async function getKeys(numberOfKeys) {
         newKeys[key] = hwid;
     }
     await updateWhitelist(newKeys);
+    return newKeys; // Return the new keys
 }
 
-module.exports = getKeys;
+// API handler
+module.exports = async (req, res) => {
+    if (req.method === 'POST') {
+        const numberOfKeys = parseInt(req.query.number, 10) || 1;
+        try {
+            const newKeys = await getKeys(numberOfKeys);
+            res.status(200).json({ keys: newKeys });
+        } catch (error) {
+            res.status(500).json({ error: error.message });
+        }
+    } else {
+        res.status(405).json({ error: 'Method not allowed' });
+    }
+};
