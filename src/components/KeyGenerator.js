a2de14edab04ce1a6566b61e90c82b50b5aecd1a
Yumi <90940961+KOKAKAKA@users.noreply.github.com>
2024-07-21 06:25:11 +0700

    Update KeyGenerator.js

--- src/components/KeyGenerator.js
+++ src/components/KeyGenerator.js
@@ -1,17 +1,10 @@
-// KeyGenerator.js
-
 import React from 'react';
 
 function KeyGenerator() {
     const handleClick = async () => {
-        try {
-            const response = await fetch('/.netlify/functions/get-keys?number=1', { method: 'POST' });
-            const result = await response.json();
-            console.log(result); // Log result
-            // Handle result (e.g., display key, update state)
-        } catch (error) {
-            console.error('Error fetching keys:', error); // Log errors
-        }
+        const response = await fetch('/.netlify/functions/get-keys?number=1', { method: 'POST' });
+        const result = await response.json();
+        console.log(result); // Handle result
     };
 
     return (
