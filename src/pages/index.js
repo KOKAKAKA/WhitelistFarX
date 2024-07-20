3478f2aef0f1845317c994e60c821b7b3fd9b950
Yumi <90940961+KOKAKAKA@users.noreply.github.com>
2024-07-20 14:27:27 +0700

    Create index.js

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import KeyInput from '../components/KeyInput';
+import KeyGenerator from '../components/KeyGenerator';
+
+const IndexPage = () => (
+  <div>
+    <h1>Whitelist System</h1>
+    <KeyInput />
+    <KeyGenerator />
+  </div>
+);
+
+export default IndexPage;
