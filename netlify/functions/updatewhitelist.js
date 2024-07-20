eacf950b2b246827f9cef23b8c736837daaf4fec
Yumi <90940961+KOKAKAKA@users.noreply.github.com>
2024-07-20 17:45:08 +0700

    Update updatewhitelist.js

--- netlify/functions/updatewhitelist.js
+++ netlify/functions/updatewhitelist.js
@@ -2,10 +2,7 @@ const fs = require('fs');
 const path = require('path');
 
 exports.handler = async (event) => {
-  console.log('Received event:', JSON.stringify(event, null, 2)); // Log the event data
-
   const { key, hwid } = JSON.parse(event.body);
-
   const whitelistPath = path.resolve(__dirname, '../whitelist.json');
 
   let whitelist;
@@ -13,18 +10,17 @@ exports.handler = async (event) => {
     const data = fs.readFileSync(whitelistPath);
     whitelist = JSON.parse(data);
   } catch (error) {
-    console.error('Failed to read whitelist:', error); // Log error
     return {
       statusCode: 500,
       body: JSON.stringify({ message: 'Failed to read whitelist' }),
     };
   }
 
-  const existingEntry = whitelist.keys.find(entry => entry.hwid === hwid);
+  const existingEntry = whitelist.keys.find(entry => entry.key === key);
   if (existingEntry) {
     return {
       statusCode: 400,
-      body: JSON.stringify({ message: 'HWID already exists' }),
+      body: JSON.stringify({ message: 'Key already exists' }),
     };
   }
 
@@ -37,7 +33,6 @@ exports.handler = async (event) => {
       body: JSON.stringify({ message: 'Whitelist updated successfully' }),
     };
   } catch (error) {
-    console.error('Failed to update whitelist:', error); // Log error
     return {
       statusCode: 500,
       body: JSON.stringify({ message: 'Failed to update whitelist' }),
