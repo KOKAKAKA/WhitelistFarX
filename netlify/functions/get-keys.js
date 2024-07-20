a7785973f00112224421fd0f74d3aaca1a6c14e9
Yumi <90940961+KOKAKAKA@users.noreply.github.com>
2024-07-20 20:37:43 +0700

    Update get-keys.js

--- netlify/functions/get-keys.js
+++ netlify/functions/get-keys.js
@@ -1,33 +1,20 @@
-const { MongoClient } = require('mongodb');
+const crypto = require('crypto');
+const saveKey = require('./save-key');
+const updateWhitelist = require('./updatewhitelist');
 
-exports.handler = async () => {
-  const uri = process.env.MONGODB_URI;
+function generateKey() {
+    return crypto.randomBytes(16).toString('hex');
+}
 
-  if (!uri) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ message: 'Missing MongoDB connection string' }),
-    };
-  }
+async function getKeys(numberOfKeys) {
+    const newKeys = {};
+    for (let i = 0; i < numberOfKeys; i++) {
+        const key = generateKey();
+        const hwid = '';
+        await saveKey(key, hwid);
+        newKeys[key] = hwid;
+    }
+    await updateWhitelist(newKeys);
+}
 
-  const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-
-  try {
-    await client.connect();
-    const database = client.db('whitelist');
-    const collection = database.collection('keys');
-
-    const keys = await collection.find({}).toArray();
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ keys }),
-    };
-  } catch (error) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ message: 'Failed to fetch keys', error: error.message }),
-    };
-  } finally {
-    await client.close();
-  }
-};
+module.exports = getKeys;
