dcc76424341e810621e6be1e65d5b88e00a439f5
Yumi <90940961+KOKAKAKA@users.noreply.github.com>
2024-07-20 14:31:23 +0700

    Create gatsby-config.js

---
+++ gatsby-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  siteMetadata: {
+    title: "Whitelist FarX",
+    description: "A site to manage whitelisting with keys and HWIDs.",
+    author: "@yourusername",
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    // Add other plugins as needed
+  ],
+};
