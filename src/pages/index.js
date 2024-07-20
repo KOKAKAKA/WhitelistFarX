7d90fb67569bf4768c32fd6d3a9eb2cef0cbf9eb
Yumi <90940961+KOKAKAKA@users.noreply.github.com>
2024-07-20 17:31:26 +0700

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import KeyInput from '../components/KeyInput';
 import KeyGenerator from '../components/KeyGenerator';
 import DisplayKeys from '../components/DisplayKeys';
 
 const IndexPage = () => (
   <div>
     <h1>Whitelist System</h1>
-    <KeyInput />
     <KeyGenerator />
     <DisplayKeys />
   </div>
