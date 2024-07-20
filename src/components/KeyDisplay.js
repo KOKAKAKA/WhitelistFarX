e5cca8b8a88e2ce866534fba4c035e68980d2901
Yumi <90940961+KOKAKAKA@users.noreply.github.com>
2024-07-21 06:12:34 +0700

    Update KeyDisplay.js

--- src/components/KeyDisplay.js
+++ src/components/KeyDisplay.js
@@ -1,19 +1,28 @@
 import React, { useEffect, useState } from 'react';
 
 function KeyDisplay() {
-    const [key, setKey] = useState('');
+    const [keys, setKeys] = useState({});
 
     useEffect(() => {
-        async function fetchKey() {
-            const response = await fetch('/api/get-key');
+        async function fetchKeys() {
+            const response = await fetch('/.netlify/functions/load-keys');
             const result = await response.json();
-            setKey(result.key);
+            setKeys(result);
         }
 
-        fetchKey();
+        fetchKeys();
     }, []);
 
-    return <div>Your Key: {key}</div>;
+    return (
+        <div>
+            <h2>Keys</h2>
+            <ul>
+                {Object.keys(keys).map(key => (
+                    <li key={key}>{key}: {keys[key]}</li>
+                ))}
+            </ul>
+        </div>
+    );
 }
 
 export default KeyDisplay;
